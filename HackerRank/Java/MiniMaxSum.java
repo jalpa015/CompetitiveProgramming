7d2be19296d8729d813f28aec2e1b0c0fff5cb62
Jalpa Patel <pjalpa015@gmail.com>
2022-05-26 10:53:15 -0400

    Hackerrank Interview preparation Kit

---
+++ HackerRank/Java/MiniMaxSum.java
@@ -0,0 +1,37 @@
+// https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one 
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class MiniMaxSum {
+
+    public static void miniMaxSum(List<Integer> arr) {
+    // sort array in place
+		Collections.sort(arr);
+		long minSum = arr.stream().limit(4).mapToLong(i -> i).sum();
+		long maxSum = arr.stream().skip(1).limit(4).mapToLong(i -> i).sum();
+		System.out.println(minSum + " " + maxSum);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
